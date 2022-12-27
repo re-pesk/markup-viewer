6a70579223bde004ea37beeab14a920ae2984c69
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-27 11:03:23 +0200

    Clear vite's config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import { loadParser } from './src/js/load-markdown-it'
 import dns from 'dns'
 dns.setDefaultResultOrder('verbatim')
 
-const markdownIt = loadParser();
-
-// console.log(plainText);
-
-function test(s) {
-  return s.toLowerCase;
-}
-
 export default defineConfig({
-  target: 'es2022',
+  target: 'esnext',
   // root: 'app',
   plugins: [
   ],
