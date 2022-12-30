8680c124e98e3e71ea5e18d7695ffb907fef5a18
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2022-12-30 17:37:20 +0200

    Add field with md source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './css/style.css';
-import Vite from './img/vite.svg?raw';
-import JS from './img/javascript.svg?raw';
+// import Vite from './img/vite.svg?raw';
+// import JS from './img/javascript.svg?raw';
 import { setupCounter } from './js/counter.js';
 import { loadParser } from './js/load-markdown-it.js'
 import JSON5 from 'json5';
@@ -20,47 +20,70 @@ async function getResponseText(url) {
 
 async function getContent() {
   const docContainer = document.createElement('div');
+  docContainer.classList.add("container");
   docContainer.innerHTML = `
-    <aside id="aside"></aside>
-    <main id="main"></main>
-  `;
-
-  const aside = docContainer.querySelector('#aside');
-
-  aside.innerHTML = `
-    <div id="toc-container">
-      <div id="toc-wrapper">
-        <div>${Vite}${JS}</div>
-        <h1>Contents</h1>
-        <p><div id="toc"></div></p>
-        <p><button id="counter" type="button"></button></p>
-        <p><div id="json"></div></p>
+    <aside>
+      <div class="container side-panel left">
+        <nav class="container">abc</nav>
+        <div class="panel-content" id="index-wrapper"></div>
       </div>
+    </aside>
+    <main>
+      <nav class="container"><div><button></button></div><div>a</div><div><button></button></div></nav>
+      <div class="container">
+        <section id="md-content"></section>
+        <section id="html-content"></section>
+      </div>
+    </main>
+    <aside>
+      <div class="side-panel right">
+        <nav class="container">abc</nav>
+        <div class="panel-content" id="toc-wrapper"></div>
+      </div>
+    </aside>
+`;
+
+  docContainer.querySelector('#index-wrapper').innerHTML = `
+    <h1>Index</h1>
+    <p>
+    <div id="index">
+      <ul>
+        <li>Vienas</li>
+        <li>Du</li>
+      </ul>
     </div>
+    </p>
   `
-  const tocContainer = aside.querySelector('#toc-container');
 
-
-  setupCounter(tocContainer.querySelector('#counter'))
+  docContainer.querySelector('#toc-wrapper').innerHTML = `
+    <h1>Contents</h1>
+    <p><div id="toc"></div></p>
+    <p><button id="counter" type="button"></button></p>
+    <p><div id="json"></div></p>
+  `
+  setupCounter(docContainer.querySelector('#counter'))
 
   const testJson5Content = await getResponseText('/content/test.json5');
-  tocContainer.querySelector('#json').innerHTML = JSON5.stringify(JSON5.parse(testJson5Content));
+  docContainer.querySelector('#json').innerHTML = JSON5.stringify(JSON5.parse(testJson5Content));
 
   let fileToLoad = '/content/index.md';
   if (window.location.search.slice(1)) {
     fileToLoad = `/content/${window.location.search.slice(1)}`;
   }
   const mdContent = await getResponseText(fileToLoad);
+
+
   const mdHtml = mdParser.render(mdContent);
 
-  tocContainer.querySelector('#toc').innerHTML = metaData.toc;
+  docContainer.querySelector('#toc').innerHTML = metaData.toc;
+  docContainer.querySelector('#md-content').innerHTML = `<pre>${mdContent}</pre>`;
 
   let frontMatter = '';
   if (metaData.frontMatter) {
     frontMatter = `<h1>${metaData.frontMatter.title}</h1><p>${metaData.frontMatter.description}</p><p>Tags: (${metaData.frontMatter.tags.join('; ')})</p>`;
   }
-  docContainer.querySelector('#main').innerHTML = `${frontMatter}\n${mdHtml}`;
-
+  docContainer.querySelector('#html-content').innerHTML = `${frontMatter}\n${mdHtml}`;
+  // docContainer.querySelector('#html-content').innerHTML = `${mdHtml}`;
 
   return docContainer;
 }
